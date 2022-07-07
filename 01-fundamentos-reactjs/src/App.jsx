10d8d692cbcf5a96e91eac5848f65a6f23c88c17
Joyce Bigoli <97766440+JoyceBigoli@users.noreply.github.com>
2022-07-07 17:52:47 -0300

    atualização

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -18,15 +18,15 @@ export function App() {
 
       <main>
         <Post 
-      author="Joyce Bigoli" 
-      content="Lorem ipsum dolor sit amet, 
+          author="Joyce Bigoli" 
+          content="Lorem ipsum dolor sit amet, 
     consectetur adipisicing elit. Vero autem impedit labore enim cum 
     natus sunt! Reiciendis sequi ratione provident repudiandae dolorem? 
     Repudiandae recusandae eos eaque inventore impedit perspiciatis error."
-    />
+      />
       <Post
-      author="Gabriel Buzzi"
-      content="Um novo post, muito legal"
+        author="Gabriel Buzzi"
+        content="Um novo post, muito legal"
       />
       </main>
     </div>
